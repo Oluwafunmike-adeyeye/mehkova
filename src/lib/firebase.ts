a8a768ce15654fdbba6699d2bdbc88d50ff92136
Oluwafunmike-adeyeye <adeyeyeoluwafunmike1@gmail.com>
2025-04-16 17:59:07 +0100

    update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,24 +2,14 @@ import { initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 
-interface FirebaseConfig {
-  apiKey: string;
-  authDomain: string;
-  projectId: string;
-  storageBucket: string;
-  messagingSenderId: string;
-  appId: string;
-  measurementId?: string;
-}
-
-const firebaseConfig: FirebaseConfig = {
-  apiKey: "AIzaSyAOE02LUyPe9hzbzA6gfnYDysA2ar8VLsw",
-  authDomain: "mehkova-86944.firebaseapp.com",
-  projectId: "mehkova-86944",
-  storageBucket: "mehkova-86944.appspot.com",
-  messagingSenderId: "596325574453",
-  appId: "1:596325574453:web:701f70b5b500c8aaa90e7c",
-  measurementId: "G-5FPF9H68E5"
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 let app: FirebaseApp;
@@ -30,7 +20,7 @@ try {
   app = initializeApp(firebaseConfig);
   auth = getAuth(app);
   db = getFirestore(app);
-  
+
   if (typeof window !== 'undefined') {
     console.log('Firebase initialized successfully');
   }
